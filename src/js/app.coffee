cf0f7c10413fcfa8937bcc01dfaad7476b12b609
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-09-15 17:35:06 +0200

    - simplify coffee script (document.ready not needed) - use lambda expressions (ES6)

--- src/js/app.coffee
+++ src/js/app.coffee
@@ -1,13 +1,11 @@
-$(document).ready( () ->
-    $(() ->
-        $('[data-toggle="tooltip"]').tooltip()
-    )
-
-  $('#menu').onePageNav
-    currentClass: "active"
-    changeHash: false
-    scrollThreshold: 0.5
-    scrollSpeed: 500
-    filter: ''
-    easing: 'swing'
+$(() ->
+  $('[data-toggle="tooltip"]').tooltip()
 )
+
+$('#menu').onePageNav
+  currentClass: "active"
+  changeHash: false
+  scrollThreshold: 0.5
+  scrollSpeed: 500
+  filter: ''
+  easing: 'swing'
--- src/js/components/AllTeams.jsx
+++ src/js/components/AllTeams.jsx
@@ -9,33 +9,33 @@ class AllTeams extends React.Component {
     }
 
     componentDidMount() {
-        $.getJSON(this.props.teamsUrl, function (result) {
+        $.getJSON(this.props.teamsUrl, (result) => {
             this.setState({teams: result.teams});
-        }.bind(this));
+        });
 
-        $.getJSON(this.props.fixturesUrl, function (result) {
+        $.getJSON(this.props.fixturesUrl, (result) => {
             this.setState({fixtures: result.fixtures});
-        }.bind(this));
+        });
     }
 
     render() {
         var groups = {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []};
         var fixturesMap = {"A": [], "B": [], "C": [], "D": [], "E": [], "F": [], "G": [], "H": []};
 
-        this.state.teams.map(function (team) {
+        this.state.teams.map((team) => {
             groups[team.group].push(team);
         });
 
         var groupKeys = Object.keys(groups);
 
-        this.state.fixtures.map(function (fixture) {
-            var team = this.state.teams.filter(function (team) {
+        this.state.fixtures.map((fixture) => {
+            var team = this.state.teams.filter((team) => {
                 return team.name === fixture.homeTeamName;
             })[0];
 
             fixture['group'] = groups[team.group];
             fixturesMap[team.group].push(fixture);
-        }.bind(this));
+        });
 
         var teamNodes = groupKeys.map(function (key) {
             return (
