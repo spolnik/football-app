f4eccea4419f79eeaab22b9c1f10252e8b2e18b1
Jacek Spólnik <jacek.spolnik@gmail.com>
2015-09-16 06:56:05 +0200

    - update fixture to start using results once they are available

--- src/js/components/Fixture.jsx
+++ src/js/components/Fixture.jsx
@@ -4,7 +4,12 @@ class Fixture extends React.Component {
 
         let matchDay = new Date(this.props.fixture.date);
         let matchDate = $.format.date(matchDay, 'dd MMMM yyyy');
-        let matchTime = $.format.date(matchDay, 'HH.mm');
+        let matchResult = $.format.date(matchDay, 'HH.mm');
+
+        if (matchDay < new Date()) {
+            matchResult =
+                `${this.props.fixture.result.goalsHomeTeam} : ${this.props.fixture.result.goalsAwayTeam}`;
+        }
 
         return (
             <li className="list-group-item">
@@ -13,7 +18,7 @@ class Fixture extends React.Component {
                     <span className="col-md-3 col-md-offset-1">{this.props.fixture.homeTeam.shortName}</span>
                     <img src={this.props.fixture.homeTeam.crestUrl} alt={this.props.fixture.homeTeam.shortName}
                          className="img-responsive match-logo col-md-1"/>
-                    <span className="col-md-2">{matchTime}</span>
+                    <span className="col-md-2">{matchResult}</span>
                     <img src={this.props.fixture.awayTeam.crestUrl} alt={this.props.fixture.awayTeam.shortName}
                          className="img-responsive match-logo col-md-1"/>
                     <span className="col-md-3">{this.props.fixture.awayTeam.shortName}</span>
