27a73685bdd9bba23efd5402c414f84743510ef3
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-09-08 09:49:01 +0200

    update 2017/2018 results

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -14,15 +14,26 @@ $("a[href*='#']").mPageScroll2id({
 });
 
 var content = document.getElementById('content');
+var content_2016 = document.getElementById('content_2016');
+var content_2017 = document.getElementById('content_2017');
 
 if (content) {
     ReactDOM.render(
         <AllTeams teamsUrl="teams.json" fixturesUrl="fixtures.json"/>,
         document.getElementById('content')
     );
-} else {
+}
+
+if (content_2016) {
     ReactDOM.render(
         <AllTeams teamsUrl="teams-2016.json" fixturesUrl="fixtures-2016.json"/>,
         document.getElementById('content_2016')
+    );
+}
+
+if (content_2017) {
+    ReactDOM.render(
+        <AllTeams teamsUrl="teams-2017.json" fixturesUrl="fixtures-2017.json"/>,
+        document.getElementById('content_2017')
     );
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ var path = require('path');
 
 module.exports = {
     entry: [
-        'webpack-hot-middleware/client',
         "./src/js/app"
     ],
     output: {
@@ -11,29 +10,25 @@ module.exports = {
         filename: "bundle.js",
         publicPath: '/assets/'
     },
-    devServer: {
-        inline: true,
-        contentBase: './assets',
-        port: 3000
-    },
-    devtool: 'cheap-module-eval-source-map',
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
-            jQuery: "jquery"
-        }),
-        new webpack.optimize.OccurenceOrderPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NoErrorsPlugin()
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        })
     ],
     module: {
-        loaders: [{
-            tests: /\.js?$/,
-            loaders: ['babel'],
-            include: path.join(__dirname, 'src', 'js')
-        }]
+        rules: [
+            {
+                test: /\.jsx?$/,
+                include: path.join(__dirname, 'src', 'js'),
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
     },
     resolve: {
-        extensions: ['', '.js', '.jsx']
+        extensions: ['.js', '.jsx']
     }
 };
